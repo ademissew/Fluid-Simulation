7a2a8dfe13d9d2da012f1f1f46bbb06680e44f9e
Alenta Demissew <alentad@mit.edu>
2018-11-26 23:12:57 -0500

    yes

---
+++ README.txt
@@ -0,0 +1,27 @@
+Hi, welcome to my code. Here are some FAQs:
+
+1. How do you compile and run my code?
+  $ cd solution
+  Then you can run the code using the following command:
+  $ ./a3 <character> <h>
+
+where the characters are e (Forward Euler), t (Trapazoidal), and r (Runge-Kutta 4), which represent the time stepping method and h is the step size.
+
+I recommend using r (or t but r is nicer looking) with a time step of 0.01.
+
+
+2. Did I collaborate with anyone in the class? If so, with whom and of what?
+Yes, I worked collaborated with Jeff Chow. We also went through the notes together, whenever one of us got stuck on something, the other would help explain a concept or help debug.
+
+3. Did you use any helpful references?
+I used the lecture slides and piazza posts.
+
+4. Are there any problems with your code/what are they?
+ Not that I know of.
+
+5. Did you do any of the extra credit? 
+  Nope.
+
+6. Comments
+I thought the pset was pretty cool and straightforward, especially once I got it working correctly!
+
---
+++ src/timestepper.cpp
@@ -0,0 +1,59 @@
+#include "timestepper.h"
+
+#include <cstdio>
+
+void ForwardEuler::takeStep(ParticleSystem* particleSystem, float stepSize)
+{
+    std::vector<Vector3f> x = particleSystem -> getState();
+    std::vector<Vector3f> f = particleSystem -> evalF(particleSystem -> getState());
+    std::vector<Vector3f> new_state;
+    for(int i=0; i<f.size(); ++i){
+        new_state.push_back(x[i]+stepSize*f[i]);
+    }
+    particleSystem -> setState(new_state);
+    
+}
+
+void Trapezoidal::takeStep(ParticleSystem* particleSystem, float stepSize)
+{
+    std::vector<Vector3f> x = particleSystem -> getState();
+    std::vector<Vector3f> f_0 = particleSystem -> evalF(particleSystem -> getState());
+    std::vector<Vector3f> new_state;
+    std::vector<Vector3f> input; 
+    for(int i=0; i<f_0.size(); ++i){
+        input.push_back(x[i]+stepSize*f_0[i]);   
+    }
+    std::vector<Vector3f> f_1 = particleSystem -> evalF(input);
+
+    for(int i=0; i<f_0.size(); ++i){
+        new_state.push_back(x[i]+stepSize*(f_0[i]+f_1[i])/2);     
+    }
+    particleSystem -> setState(new_state);
+}
+
+
+void RK4::takeStep(ParticleSystem* particleSystem, float stepSize)
+{
+    std::vector<Vector3f> x = particleSystem -> getState();
+    std::vector<Vector3f> k_1 = particleSystem -> evalF(particleSystem -> getState());
+    std::vector<Vector3f> new_state;
+    std::vector<Vector3f> input1,input2,input3; 
+    for(int i=0; i<x.size(); ++i){
+        input1.push_back(x[i]+stepSize*k_1[i]/2);
+    } 
+    std::vector<Vector3f> k_2 = particleSystem -> evalF(input1);
+    for(int i=0; i<x.size(); ++i){
+        input2.push_back(x[i]+stepSize*k_2[i]/2);
+    }
+    std::vector<Vector3f> k_3 = particleSystem -> evalF(input2);
+    for(int i=0; i<x.size(); ++i){
+        input3.push_back(x[i]+stepSize*k_3[i]);
+    }
+    std::vector<Vector3f> k_4 = particleSystem -> evalF(input3);
+    for(int i=0; i<x.size(); ++i){
+        new_state.push_back(x[i]+(stepSize/6)*(k_1[i]+2*k_2[i]+2*k_3[i]+k_4[i]));
+    }
+    particleSystem -> setState(new_state);
+
+}
+
